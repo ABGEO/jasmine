8b5731a36bcf83f3e1d2a95fe85c18ab90722132
ABGEO <takalandzet@gmail.com>
2024-03-25 01:31:36 +0400

    chore(flora-bridge): add hostname suffix to MQTT clinet ID

--- packages/flora-bridge/broker/broker.go
+++ packages/flora-bridge/broker/broker.go
@@ -2,6 +2,7 @@ package broker
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/abgeo/jasmine/packages/flora-bridge/config"
@@ -11,16 +12,21 @@ import (
 const connectionTimeout = 5 * time.Second
 
 func NewBroker(conf *config.Config) (mqtt.Client, error) {
+	hostname, err := os.Hostname()
+	if err != nil {
+		return nil, fmt.Errorf("unable to get hostname: %w", err)
+	}
+
 	opts := mqtt.NewClientOptions().
 		SetAutoReconnect(true).
 		AddBroker(conf.MQTT.Broker).
-		SetClientID(conf.MQTT.User).
+		SetClientID(fmt.Sprintf("%s@%s", conf.MQTT.User, hostname)).
 		SetUsername(conf.MQTT.User).
 		SetPassword(conf.MQTT.Password)
 
 	client := mqtt.NewClient(opts)
 	if token := client.Connect(); token.WaitTimeout(connectionTimeout) && token.Error() != nil {
-		return nil, fmt.Errorf("unable to connect to MQTT server: %w", fmt.Errorf("unable to update User: %w", token.Error()))
+		return nil, fmt.Errorf("unable to connect to MQTT server: %w", token.Error())
 	}
 
 	return client, nil
