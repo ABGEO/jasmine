4736b3ae80afdadb4a64ec2bf5769f101861cec2
ABGEO <takalandzet@gmail.com>
2024-05-06 00:39:14 +0400

    style(flora-bloom): change primary color

--- packages/flora-bloom/src/theme.ts
+++ packages/flora-bloom/src/theme.ts
@@ -1,7 +1,23 @@
 "use client";
 
-import { createTheme } from "@mantine/core";
+import { colorsTuple, createTheme } from "@mantine/core";
 
-const theme = createTheme({});
+const theme = createTheme({
+  primaryColor: "jasmine",
+  colors: {
+    jasmine: colorsTuple([
+      "#e9fcf1",
+      "#d8f4e5",
+      "#b5e6cb",
+      "#8cd8af",
+      "#6bcb96",
+      "#56c586",
+      "#49c17e",
+      "#39aa6b",
+      "#2e975e",
+      "#1d834e",
+    ]),
+  },
+});
 
 export { theme };
