c14fdd976aeb15d9c2d3d279b94c65e4edd13760
ABGEO <takalandzet@gmail.com>
2024-05-06 16:25:40 +0400

    build(flora-bloom): add NEXT_PUBLIC_S3_URL variable to entrypoint

--- deploy/docker/flora-bloom/docker-entrypoint.sh
+++ deploy/docker/flora-bloom/docker-entrypoint.sh
@@ -4,5 +4,6 @@ set -e
 
 grep -rwl ".next" -e "_NEXT_PUBLIC_GRAFANA_URL_" | xargs -r sed -i "s!_NEXT_PUBLIC_GRAFANA_URL_!${NEXT_PUBLIC_GRAFANA_URL}!g"
 grep -rwl ".next" -e "_NEXT_PUBLIC_FLORA_BRIDGE_URL_" | xargs -r sed -i "s!_NEXT_PUBLIC_FLORA_BRIDGE_URL_!${NEXT_PUBLIC_FLORA_BRIDGE_URL}!g"
+grep -rwl ".next" -e "_NEXT_PUBLIC_S3_URL_" | xargs -r sed -i "s!_NEXT_PUBLIC_S3_URL_!${NEXT_PUBLIC_S3_URL}!g"
 
 exec "$@"
